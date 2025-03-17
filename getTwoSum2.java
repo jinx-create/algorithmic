2bc25620a248226e5418a8d5dc954a0732d9fd4e
jinx-create <2948854044@qq.com>
2025-03-17 14:46:53 +0800

    Update getTwoSum.java

--- getTwoSum2.java
+++ getTwoSum2.java
@@ -1,7 +1,19 @@
 package getSum;
 
-public class getTwoSum2 {
-    public static void main(String[] args) {
+public static void main(String[] args) {
+        int[] num = {2,7,11,15};
+        int target = 9;
 
+        System.out.println(record(9,num));
+    }
+    public static int[] record(int target ,int[] num){
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 1; j < num.length; j++) {
+                if(num[i] + num[j] == target ){
+                   return new int[]{i,j};
+                }
+            }
+        }
+        return new int[0];
     }
 }
